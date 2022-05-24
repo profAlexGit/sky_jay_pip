2380c6325587507d4afbc64ecc2eaf8c8a78d0c4
katyaruch <katya.ruch@gmail.com>
2022-05-24 12:16:29 +0300

    добавлена виртуальная клавиатура к поиску (simple-keyboard js)

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,51 @@
 (() => {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // Bootstrap validation
   const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
       }
-
       form.classList.add('was-validated')
     }, false)
   })
 })()
+
+// Virtual Keyboard
+const Keyboard = window.SimpleKeyboard.default;
+const KeyboardLayouts = window.SimpleKeyboardLayouts.default;
+const layout = new KeyboardLayouts().get("russian");
+
+const myKeyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+   ...layout,
+   physicalKeyboardHighlight: true,
+});
+
+document.querySelector(".form-control-search").addEventListener("input", event => {
+  myKeyboard.setInput(event.target.value);
+});
+
+function onChange(input) {
+  document.querySelector(".form-control-search").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+  if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+  let currentLayout = myKeyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  console.log(currentLayout);
+  myKeyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
