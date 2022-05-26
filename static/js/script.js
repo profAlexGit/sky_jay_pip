fbec03c368139016ea55417ef79eeb2dd9dc3189
katyaruch <katya.ruch@gmail.com>
2022-05-26 19:32:09 +0300

    окно со стартовым видео + правки адаптива

--- static/js/script.js
+++ static/js/script.js
@@ -49,3 +49,30 @@ function handleShift() {
     layoutName: shiftToggle
   });
 }
+
+// lazyload
+  document.addEventListener("DOMContentLoaded", function() {
+    var lazyVideos = [].slice.call(document.querySelectorAll("video.lazyload"));
+    if ("IntersectionObserver" in window) {
+      var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(video) {
+          if (video.isIntersecting) {
+            for (var source in video.target.children) {
+              var videoSource = video.target.children[source];
+              if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                videoSource.src = videoSource.dataset.src;
+              }
+            }
+
+            video.target.load();
+            video.target.classList.remove("lazyload");
+            lazyVideoObserver.unobserve(video.target);
+          }
+        });
+      });
+
+      lazyVideos.forEach(function(lazyVideo) {
+        lazyVideoObserver.observe(lazyVideo);
+      });
+    }
+  });
