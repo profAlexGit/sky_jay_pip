681d5a88f933e310304f25aaf1a210560f424b53
katyaruch <katya.ruch@gmail.com>
2022-05-30 00:40:41 +0300

    добавлена верстка тегов и списка видео на главной

--- static/js/script.js
+++ static/js/script.js
@@ -82,3 +82,10 @@ function handleShift() {
       });
     }
   });
+
+
+//  tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
