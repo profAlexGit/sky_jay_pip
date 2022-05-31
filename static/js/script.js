3f8a6d0db91c4baf98d82cf43f843361dbeeddb9
katyaruch <katya.ruch@gmail.com>
2022-06-01 01:19:17 +0300

    добавлен шаблон видео-плеера в окне

--- static/js/script.js
+++ static/js/script.js
@@ -27,7 +27,7 @@ Array.from(inputsDate).forEach(input => {
 
 // modal UserType
 if(window.location.pathname === '/'){
-  var modalUserType = new bootstrap.Modal(document.getElementById('modal-user-type'));
+  const modalUserType = new bootstrap.Modal(document.getElementById('modal-user-type'));
   modalUserType.show();
 }
 
@@ -69,13 +69,13 @@ function handleShift() {
 
 // lazyload
   document.addEventListener("DOMContentLoaded", function() {
-    var lazyVideos = [].slice.call(document.querySelectorAll("video.lazyload"));
+    const lazyVideos = [].slice.call(document.querySelectorAll("video.lazyload"));
     if ("IntersectionObserver" in window) {
-      var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+      const lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
         entries.forEach(function(video) {
           if (video.isIntersecting) {
-            for (var source in video.target.children) {
-              var videoSource = video.target.children[source];
+            for (const source in video.target.children) {
+              const videoSource = video.target.children[source];
               if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
                 videoSource.src = videoSource.dataset.src;
               }
@@ -95,19 +95,49 @@ function handleShift() {
   });
 
 //  tooltips
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
 
 // videoPlayer
-  var videoPlayer = document.getElementById('video-player');
-  var video = videoPlayer.querySelector('video');
-  var videoPlay = videoPlayer.querySelector('.video-player__play');
+  const videoPlayers = document.querySelectorAll('.video-player');
+  Array.from(videoPlayers).forEach(videoPlayer => {
+    const video = videoPlayer.querySelector('video');
+    const videoPlay = videoPlayer.querySelector('.video-player__play');
 
-  videoPlay.addEventListener("click", function() {
-    if (video.paused || video.ended) video.play();
-    else video.pause();
+    videoPlay.addEventListener("click", function() {
+      if (video.paused || video.ended) video.play();
+      else video.pause();
 
-    videoPlay.setAttribute('data-state','hidden');
+      videoPlay.setAttribute('data-state','hidden');
+    });
+  });
+  
+  const itemsVideo = document.querySelectorAll('.video-item');
+  Array.from(itemsVideo).forEach(itemVideo => {
+    const itemVideoCover = itemVideo.querySelector('.video-item__cover');
+    itemVideoCover.addEventListener('click', function(e) {
+      e.preventDefault();
+      const modalVideo = new bootstrap.Modal(document.getElementById('modal-video'));
+      modalVideo.show();
+    });
+  });
+
+  // inputs Range
+  const inputsRange = document.querySelectorAll('input[type="range"]');
+
+  function handleInputChange(e) {
+    let target = e.target;
+    const min = target.min;
+    const max = target.max;
+    const val = target.value;
+
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+
+    console.log('change')
+  };
+
+  Array.from(inputsRange).forEach(input => {
+    input.addEventListener('change', handleInputChange);
   });
