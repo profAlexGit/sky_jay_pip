187711adf4889bcdd860f0cc7882d1aa34348f8b
katyaruch <katya.ruch@gmail.com>
2022-06-02 12:12:53 +0300

    добавлена генерация sourceMap для scss

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,32 +1,48 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
 
-module.exports = {
-   entry: '/app.js',
-   mode: 'production',
-   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
-   },
-   module: {
-      rules: [{
-         test:/\.(s*)css$/,
-         use: [
-            miniCss.loader,
-            'css-loader',
-            'resolve-url-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true, // <-- !!IMPORTANT!!
-              }
+var config = {
+  entry: '/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+       test:/\.(s*)css$/,
+       use: [
+          miniCss.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
             }
-         ]
-      }]
-   },
-   plugins: [
-      new miniCss({
-         filename: 'style.css',
-      }),
-   ]
+          },
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true, // <-- !!IMPORTANT!!
+            }
+          }
+       ]
+    }]
+  },
+  plugins: [
+    new miniCss({
+       filename: 'style.css',
+    }),
+  ]
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+
+  if (argv.mode === 'production') {
+    //...
+  }
+
+  return config;
 };
