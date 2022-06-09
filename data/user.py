4ebeecb7f67f0ac29e48ea3fcdbb924dd5f401bc
repin <vrepina2000@gmail.com>
2022-06-09 15:45:36 +0300

    дополнение к предыдущему коммиту

--- data/user.py
+++ data/user.py
@@ -1,41 +1,26 @@
-import sqlalchemy as sa
+import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
-    __tablename__ = 'user'
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    isUser = sa.Column(sa.Boolean, nullable=False)
-    name = sa.Column(sa.String(50), nullable=False)
-    surname = sa.Column(sa.String(60), nullable=False)
-    dateBirth = sa.Column(sa.String(20), nullable=False)
-    placeResidence = sa.Column(sa.String(100), default='None')
-    password = sa.Column(sa.BLOB, nullable=False)
-    email = sa.Column(sa.String(100), nullable=False)
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    isUser = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(50), nullable=False)
+    surname = sqlalchemy.Column(sqlalchemy.String(60), nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String(20),
+                              index=True, unique=True, nullable=False)
+    date_birth = sqlalchemy.Column(sqlalchemy.String(20), nullable=False)
+    place_residence = sqlalchemy.Column(sqlalchemy.String(100), default='None')
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-def createUserModel(_isUser: bool = True) -> User:
-    """
-    Функция создающая экземпляр класса User и собирающая данные с формы регистрации
-    для внесения данных в БД
-
-    _isUser - Параметр отвечающий за определение пользователя. Если False то психолог, иначе пользователь
-    """
-
-    from werkzeug.security import generate_password_hash
-    from flask import request
-
-    password = generate_password_hash(request.form['password']).encode('utf-8')
-    name = request.form['name']
-    surname = request.form['surname']
-    placeResidence = request.form['placeResidence']
-    dateBirth = request.form['dateBirth']
-    email = request.form['email']
-
-    user = User(
-        isUser=_isUser, name=name, surname=surname, placeResidence=placeResidence, dateBirth=dateBirth,
-        password=password, email=email)
-
-    return user
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
