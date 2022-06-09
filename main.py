ad28756b599390518783b607f6b3f9bdb265c02a
repin <vrepina2000@gmail.com>
2022-06-09 15:01:56 +0300

    изменены обработчики для входа и регистрации

--- main.py
+++ main.py
@@ -4,50 +4,76 @@ from flask_login import LoginManager, login_user, current_user, login_required, 
 from werkzeug.utils import redirect
 
 from data import db_session, user
+from data.user import User
+from forms.login_form import LoginForm
+from forms.register_form import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sky_jay_secret_key'
-
-
-@app.route('/registration')  # Страница которая перенаправляет на регистрацию пользователя или психолога
-def registrationStage_1():
-    return render_template('auth/registration_page1.html')
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/registration/psychologist', methods=['GET', 'POST'])  # Страница регистрации психолога
-def registrationStage_2_Psycho():
-    if request.method == 'GET':
-        return 'Здесь будет страница регистрации психолога'
-    elif request.method == 'POST':
-        user1 = user.createUserModel(_isUser=False)
-        if not (user is None):
-            session = db_session.create_session()
-            session.add(user1)
-            session.commit()
-            session.close()
-            return redirect('/all-users')
-        else:  # Тестовая страница
-            return \
-                '<!DOCTYPE html><div style="background-color: gray;">' \
-                '<h2>Такой пользователь уже существует!</h2></div>' \
-                '<a href="/all-users">Просмотреть всех пользователей</a>'
+def registration_psychologist():
+    pass
 
 
-@app.route('/registration/user', methods=['GET', 'POST'])  # Страница регистрации пользователя
-def registrationStage_2_User():
-    if request.method == 'GET':
-        return render_template('auth/registration.html')
-    elif request.method == 'POST':
-        user1 = user.createUserModel(_isUser=True)
-        session = db_session.create_session()
-        session.add(user1)
-        session.commit()
-        session.close()
-        return redirect('/all-users')
+@app.route('/registration/user', methods=['GET', 'POST'])
+def registration_user():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+            date_birth=form.date_birth.data,
+            place_residence=form.place_residence.data,
+            isUser=False,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
-@app.route('/login')  # Страница входа
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect("/")
+        return render_template('auth/login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('auth/login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
