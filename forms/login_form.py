ec62db82f14f1e74bca76c1d21eeb3fa2d864359
repin <vrepina2000@gmail.com>
2022-06-09 16:14:19 +0300

    поправлена форма входа и изменен login.html

--- forms/login_form.py
+++ forms/login_form.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
+    email = EmailField('Почта')
+    password = PasswordField('Пароль')
     submit = SubmitField('Войти')
